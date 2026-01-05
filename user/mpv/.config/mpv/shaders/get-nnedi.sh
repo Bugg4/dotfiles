98f5ec085a13395f996c6262e9be441c499a9adb
Bugg4 <bulgarelli.marco99@gmail.com>
2026-01-05 13:37:05 +0100

    Move all user dotfiles to user subdir

---
+++ user/mpv/.config/mpv/shaders/get-nnedi.sh
@@ -0,0 +1,8 @@
+wget https://raw.githubusercontent.com/bjin/mpv-prescalers/refs/heads/master/nnedi3-nns128-win8x4.hook
+wget https://raw.githubusercontent.com/bjin/mpv-prescalers/refs/heads/master/nnedi3-nns128-win8x6.hook
+wget https://raw.githubusercontent.com/bjin/mpv-prescalers/refs/heads/master/nnedi3-nns64-win8x4.hook
+wget https://raw.githubusercontent.com/bjin/mpv-prescalers/refs/heads/master/nnedi3-nns64-win8x6.hook
+wget https://raw.githubusercontent.com/bjin/mpv-prescalers/refs/heads/master/nnedi3-nns32-win8x4.hook
+wget https://raw.githubusercontent.com/bjin/mpv-prescalers/refs/heads/master/nnedi3-nns32-win8x6.hook
+wget https://raw.githubusercontent.com/bjin/mpv-prescalers/refs/heads/master/nnedi3-nns16-win8x4.hook
+wget https://raw.githubusercontent.com/bjin/mpv-prescalers/refs/heads/master/nnedi3-nns16-win8x6.hook
