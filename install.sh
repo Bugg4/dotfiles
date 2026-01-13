5eade4741755dcefd5b3117aa99d1aa54ee12591
Bugg4 <bulgarelli.marco99@gmail.com>
2026-01-13 21:55:25 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-sudo pacman -Sy --noconfirm stow git fastfetch
-stow --no-folding --verbose --dir="${HOME}/dotfiles/" --target="${HOME}/" --adopt ./*
+# Ask for sudo upfront (needed for pacman)
+sudo pacman -Sy --noconfirm stow fastfetch
+
+# Navigate to the dotfiles directory
+cd "${HOME}/dotfiles" || { echo "Dotfiles directory not found!"; exit 1; }
+
+# Stow and Restore
+stow --no-folding --verbose --target="${HOME}" --adopt */ && \
 git restore .
+
+# Final
 source "${HOME}/.bashrc"
